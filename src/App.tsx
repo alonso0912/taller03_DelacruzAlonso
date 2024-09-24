6bde25a404493b26bb1be9d9a3c2f7a263ef49ae
Romero <sromeroar20@gmail.com>
2024-09-24 12:57:26 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import miImagen from './assets/junior.jpg'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src={miImagen} alt="junior de Barranquilla" />
+        <h1>Junior 100</h1>
+        <p>El juniior de barranquilla cumplio 100 año el 7 de agosto</p>
+      </div>
+    </>
+  )
+}
+
+export default App
